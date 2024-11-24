d4e0aaa35962b633912815b75822be73a342919e
Abhijit <121245953+abhijit-gore24@users.noreply.github.com>
2024-11-24 15:34:42 +0530

    Namaste react 06
    Namaste react - 06

---
+++ app1 2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// JSX React
+const jsxHeading = <h1>This is h1 tag using JSX 🚀</h1> // This is not HTML this is JSX. JSX is different than HTML its HTML like syntax
+const root = ReactDOM.createRoot(document.getElementById("root2"))
+
+root.render(jsxHeading)
---
+++ restaurantData.js
@@ -0,0 +1,64 @@
+const restaurantData = [
+  {
+    resName: "Kakke Da Paratha",
+    cuisine: "Biryani, North Indian, Asian",
+    rating: "4.4",
+    deliveryTime: "38",
+  },
+  {
+    resName: "Chai Shai",
+    cuisine: "Tea, Snacks",
+    rating: "4.7",
+    deliveryTime: "20",
+  },
+  {
+    resName: "The Tandoor",
+    cuisine: "Indian, BBQ",
+    rating: "4.5",
+    deliveryTime: "45",
+  },
+  {
+    resName: "Pizza Planet",
+    cuisine: "Pizza, Fast Food",
+    rating: "4.2",
+    deliveryTime: "25",
+  },
+  {
+    resName: "Sushi Haven",
+    cuisine: "Japanese, Sushi",
+    rating: "4.8",
+    deliveryTime: "30",
+  },
+  {
+    resName: "The Burger Joint",
+    cuisine: "Burgers, Fast Food",
+    rating: "4.3",
+    deliveryTime: "20",
+  },
+  {
+    resName: "Pasta Paradise",
+    cuisine: "Pasta, Italian",
+    rating: "4.6",
+    deliveryTime: "35",
+  },
+  {
+    resName: "Vegan Vibes",
+    cuisine: "Vegan, Organic",
+    rating: "4.7",
+    deliveryTime: "40",
+  },
+  {
+    resName: "Curry House",
+    cuisine: "Indian, Curry",
+    rating: "4.1",
+    deliveryTime: "50",
+  },
+  {
+    resName: "Dosa Delight",
+    cuisine: "South Indian, Dosa",
+    rating: "4.2",
+    deliveryTime: "30",
+  },
+];
+
+export default restaurantData;
